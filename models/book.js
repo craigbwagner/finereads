3a83f7c4f1f3269d9fd4e77d6ab8a8a6d25363e1
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-06-14 12:16:08 -0400

    add new fields to book schema

--- models/book.js
+++ models/book.js
@@ -1,11 +1,24 @@
 const mongoose = require('mongoose');
 
-const bookSchema = mongoose.Schema({
-	title: { type: String, required: true },
-	status: String,
-	rating: Number,
-	dateAdded: Date,
-});
+const bookSchema = mongoose.Schema(
+	{
+		googleBooksId: { type: String, required: true },
+		title: { type: String, required: true },
+		authors: { type: [String], required: true },
+		averageRating: Number,
+		imageLink: String,
+		isbn10: String,
+		isbn13: String,
+		publisher: String,
+		publishedDate: String,
+		pageCount: Number,
+		description: String,
+		shelf: { type: String, default: 'to-read', enum: ['to-read', 'reading', 'read'] },
+		userRating: Number,
+		googleBooksURL: String,
+	},
+	{ timestamps: { updatedAt: 'updated_at', createdAt: 'created_at' } }
+);
 
 const Book = mongoose.model('Book', bookSchema);
 
