4ce19416ceddf5985804bbd7dc77ec8e1622714a
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-06-14 12:52:55 -0400

    update user schema to accept an array of embedded data as books, add bookSchema to user.js

--- models/user.js
+++ models/user.js
@@ -1,5 +1,26 @@
 const mongoose = require('mongoose');
 
+const bookSchema = mongoose.Schema(
+	{
+		googleBooksId: { type: String, required: true },
+		// title: { type: String, required: true },
+		// authors: { type: [String], required: true },
+		// averageRating: Number,
+		// imageLink: String,
+		// isbn10: String,
+		// isbn13: String,
+		// publisher: String,
+		// publishedDate: String,
+		// pageCount: Number,
+		// description: String,
+		shelf: { type: String, default: 'to-read', enum: ['to-read', 'reading', 'read'] },
+		userRating: Number,
+		userReview: String,
+		// googleBooksURL: String,
+	},
+	{ timestamps: { updatedAt: 'updated_at', createdAt: 'created_at' } }
+);
+
 const userSchema = mongoose.Schema({
 	username: {
 		type: String,
@@ -9,12 +30,7 @@ const userSchema = mongoose.Schema({
 		type: String,
 		required: true,
 	},
-	shelvedBooks: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Book',
-		},
-	],
+	shelvedBooks: [bookSchema],
 });
 
 const User = mongoose.model('User', userSchema);
